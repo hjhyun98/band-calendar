a37d64ee3a639efaa1381fda6e3c1b8216e8afef
hyunjo <hyunjo@hyunjoui-MacBookAir.local>
2026-05-17 12:15:18 +0900

    add local schedule save and calendar highlights

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,34 @@
 "use client";
 
-import {useState} from "react";
+import { loadBindings } from "next/dist/build/swc";
+import { useEffect, useState } from "react";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 import { start } from "repl";
 
 const times = ["9:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00", "24:00"];
 
+type Schedule ={
+  startIndex: number;
+  endIndex: number | null;
+};
+
 export default function Home(){
+  
+  useEffect(()=> {
+    const savedData = localStorage.getItem("schedules");
+
+    if (!savedData) return;
+
+    const schedules: Record<string, Schedule> = JSON.parse(savedData);
+    setSavedDateKeys(Object.keys(schedules));
+  }, []);
+  
   const [selectedDate, setSelectedDate] = useState<Date | null>(null);
   const maxDate = new Date();
   maxDate.setMonth(maxDate.getMonth() + 6);
+
+  const [savedDateKeys, setSavedDateKeys] =useState<string[]>([]);
 
   const formatDate = (date: Date) => {
     const weekday = date.toLocaleDateString("en-US", {
@@ -73,6 +91,49 @@ export default function Home(){
     
   }
 
+  const handleSave = () => {
+    if(!selectedDate || startIndex === null) return;
+
+    const dateKey = formatDate(selectedDate);
+
+    const savedData = localStorage.getItem("schedules");
+
+    const schedules: Record<string, Schedule> = savedData
+      ? JSON.parse(savedData)
+      : {};
+
+      schedules[dateKey] = {
+        startIndex,
+        endIndex,
+      };
+
+      localStorage.setItem("schedules", JSON.stringify(schedules));
+      setSavedDateKeys(Object.keys(schedules));
+
+      console.log("저장완료:", schedules);
+  };
+
+  const handleReset = () => {
+    if (!selectedDate) return;
+
+    const dateKey = formatDate(selectedDate);
+    const savedData = localStorage.getItem("schedules");
+
+    if (!savedData) return;
+
+    const schedules: Record<string, Schedule> = JSON.parse(savedData);
+
+    delete schedules[dateKey];
+
+    localStorage.setItem("schedules", JSON.stringify(schedules));
+    setSavedDateKeys(Object.keys(schedules));
+
+    setStartIndex(null);
+    setEndIndex(null);
+
+    console.log("삭제완료:",schedules);
+  };
+
   const selectedTimeText = 
     startIndex === null
     ? "-"
@@ -86,13 +147,42 @@ export default function Home(){
 
       <Calendar
         onChange={(date) => {
-          setSelectedDate(date as Date)
-          setStartIndex(null);
-          setEndIndex(null);
-        }}
+          const clickedDate = date as Date;
+          setSelectedDate(clickedDate);
+
+          const dateKey = formatDate(clickedDate);
+          const savedData = localStorage.getItem("schedules");
+
+          if(!savedData) {
+            setStartIndex(null);
+            setEndIndex(null);
+            return;
+          }
+
+          const schedules: Record<string, Schedule> = JSON.parse(savedData);
+          const savedSchedule = schedules[dateKey];
+        
+          if (savedSchedule) {
+            setStartIndex(savedSchedule.startIndex);
+            setEndIndex(savedSchedule.endIndex);
+          } else {
+            setStartIndex(null);
+            setEndIndex(null);
+          }
+        }
+      }
         value={selectedDate}
         minDate={new Date()}
         maxDate={maxDate}
+        tileClassName={({date}) => {
+          const dateKey = formatDate(date);
+
+          if(savedDateKeys.includes(dateKey)){
+            return "saved-date";
+          }
+
+          return null;
+        }}
       />
 
       <div>
@@ -110,26 +200,22 @@ export default function Home(){
           </p>
 
           <button
+            disabled={!selectedDate}
+            onClick={handleReset}
+            className="ml-auto px-4 py-2 rounded bg-gray-200 text-black"
+          >reset</button>
+
+          <button
             disabled={startIndex === null}
-            onClick={() => {
-              console.log("저장할 데이터:", {
-                date: selectedDate,
-                startIndex,
-                endIndex,
-                startTime: startIndex !== null ? times[startIndex] : null,
-                endTime: endIndex !== null ? times[endIndex+1] : null,
-              });
-            }}
+            onClick={handleSave}
             className={`
-              ml-auto px-4 py-2 rounded
+              ml-2 px-4 py-2 rounded
               ${
                 startIndex === null
                   ? "bg-gray-300 text-gray-500"
                   : "bg-blue-500 text-white"
               }`}
-          >
-            저장
-          </button>
+          >save</button>
         </div>
 
           <div className="grid grid-cols-4 gap-4">
